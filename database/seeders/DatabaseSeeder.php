212a8cfc3beb27cf53437247ff468e47cedf03b7
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-08-26 00:16:43 +0100

    feat: Add seeder and Job seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\Tag;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +19,19 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test',
+            'last_name' => 'phpUser',
             'email' => 'test@example.com',
         ]);
+
+        //   Job::factory(200)->create(); logic moved to the JobnSeeder
+        $this->call(JobSeeder::class);
+        Post::factory(200)->create();
+        Tag::factory(200)->create();
+
+        // you can create dedicated seeders for each model
+        // $this->call([JobSeeder::class, PostSeeder::class, TagSeeder::class]);
+
+        //we can seed specific class by using php artisan db:seed --class=ClassName
     }
 }
