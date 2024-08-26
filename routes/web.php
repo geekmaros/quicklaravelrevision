324865a1275aeec5670149e0482cd4865af38e36
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-08-26 22:41:47 +0100

    feat: update the app routes

--- routes/web.php
+++ routes/web.php
@@ -8,25 +8,94 @@ Route::get('/', function () {
 
 });
 
+// Index
 Route::get('/jobs', function () {
     //we replaced Job::all() with Job::with('employer')->get() so as to eager load the employer relationship
     // and reduce the number of queries
 
     //$jobs = Job::with('employer')->get();
     //cursor based pagination is best for large datasets
-    $jobs = Job::with('employer')->simplePaginate(3);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs,
     ]);
 });
+
+// Create
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
+// Show
 Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
 
-    return view('job', [
+    return view('jobs.show', [
         'job' => $job,
     ]);
+});
+
+// Store
+Route::post('/jobs', function () {
+    //validation
+
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+
+    $job = Job::find($id);
+
+    return view('jobs.edit', [
+        'job' => $job,
+    ]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    //validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+    // authorize (on hold)
+    //update the job
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    // persist
+    //redirect to the Job page
+    return redirect('/jobs/'.$job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    // validate
+    // authorize
+
+    //delete
+    $job = Job::findOrFail($id);
+    $job->delete();
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
