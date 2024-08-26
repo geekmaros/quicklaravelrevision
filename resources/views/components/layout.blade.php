3f3f43183cb3bc3a211aef58179297ee961b2e28
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-08-26 22:41:00 +0100

    feat: update styling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -104,8 +104,10 @@
 
 
 <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-7xl px-4 sm:flex sm:justify-between items-center py-6 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
+        
+        <x-button href="/jobs/create">Create New Job</x-button>
     </div>
 </header>
 <main class="">
