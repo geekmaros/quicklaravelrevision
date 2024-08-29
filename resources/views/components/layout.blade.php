ca0a1e8ec29355cba77bd7f3ae7bf88aaa813856
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-08-29 18:38:12 +0100

    feat: add links to layouts

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -58,31 +58,11 @@
                 </div>
             </div>
             <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button type="button"
-                        class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                    <span class="absolute -inset-1.5"></span>
-                    <span class="sr-only">View notifications</span>
-                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                         aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"/>
-                    </svg>
-                </button>
-
-                <!-- Profile dropdown -->
-                <div class="relative ml-3">
-                    <div>
-                        <button type="button"
-                                class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                            <span class="absolute -inset-1.5"></span>
-                            <span class="sr-only">Open user menu</span>
-                            <img class="h-8 w-8 rounded-full"
-                                 src="https:/laracasts.com/images/lary-ai-face.svg"
-                                 alt="">
-                        </button>
-                    </div>
-                </div>
+                @guest
+                    <x-nav-link type="link" href="/login" :active="request()->is('login')">Login</x-nav-link>
+                    <x-nav-link type="link" href="/register" :active="request()->is('register')">Register
+                    </x-nav-link>
+                @endguest
             </div>
         </div>
     </div>
@@ -106,7 +86,7 @@
 <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl px-4 sm:flex sm:justify-between items-center py-6 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
-        
+
         <x-button href="/jobs/create">Create New Job</x-button>
     </div>
 </header>
