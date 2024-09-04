1722b559228f30cd31e4776949796ef73911cdf6
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-09-04 23:05:48 +0100

    feat: add login controller

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
 class SessionController extends Controller
 {
     //
@@ -14,9 +17,26 @@ class SessionController extends Controller
 
     public function store()
     {
-        //        dd('create');
+        //        validate
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
 
-        //        return view('auth.register');
-        dd(request()->all());
+        // attempt to authenticate the user
+
+        // if not, redirect back
+        if (! Auth::attempt($attributes)) {
+            // if it fails it redirects back
+            throw ValidationException::withMessages([
+                'email' => 'Sorry, invalid credentials',
+            ]);
+        }
+
+        // regenerate the session
+        request()->session()->regenerate();
+
+        // redirect
+        return redirect('/jobs');
     }
 }
