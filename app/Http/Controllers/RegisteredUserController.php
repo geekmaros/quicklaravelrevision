b78ffb844c79fd5f4aef673dae0427de55d5ba4d
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-09-04 23:05:40 +0100

    feat: add register controller

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,8 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+
 class RegisteredUserController extends Controller
 {
+    public function store()
+    {
+        //       validate
+        $attributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email', 'max:254'],
+            'password' => ['required', Password::min(6), 'confirmed'], // confirmed = password_confirmation
+        ]);
+
+        //        dd($attributes);
+        //     create the user
+        $user = User::create($attributes);
+
+        //     sign the user in
+        Auth::login($user);
+
+        //     redirect
+        return redirect('/jobs');
+
+    }
+
     public function create()
     {
         //        dd('create');
@@ -11,11 +37,11 @@ class RegisteredUserController extends Controller
         return view('auth.register');
     }
 
-    public function store()
+    public function destroy()
     {
-        //        dd('create');
 
-        //        return view('auth.register');
-        dd(request()->all());
+        Auth::logout();
+
+        return redirect('/');
     }
 }
