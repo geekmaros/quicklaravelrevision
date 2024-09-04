f93813b2b7e8cc6b6ef865e3402b011a50fb709b
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-09-04 23:06:43 +0100

    feat: add protected guarded to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,13 +11,16 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    protected $guarded = [];
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
